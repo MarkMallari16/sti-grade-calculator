2a9b6f76dc215a31529972b1f1808f690a78bddf
Mark Christian Mallari <mallariftw01@gmail.com>
2025-12-14 12:42:58 +0800

    refactor: Code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useRef, useState } from 'react'
 
-import './App.css'
-
 type Grades = {
   prelims: "",
   midterm: "",
@@ -86,13 +84,14 @@ function App() {
               {/* Sidebar toggle icon */}
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
             </label>
-            <div className="px-4 font-bold">STI Grade Calculator</div>
+            <div className="px-4 font-bold">
+              STI Grade Calculator</div>
           </nav>
           {/* Page content here */}
-          <div className='min-h-screen grid place-items-center lg:p-0 p-4'>
+          <div className='min-h-screen grid grid-cols-2  m-4'>
             <div>
               {/**Content */}
-              <div className='ring ring-inset ring-base-300 rounded-md p-8 lg:w-xl'>
+              <div className='ring ring-inset ring-base-300 rounded-md p-10 w-lg'>
                 <div className='mb-4'>
                   <h1 className='text-2xl font-bold'>STI College Grades Calculator</h1>
                   <p>
@@ -130,6 +129,7 @@ function App() {
               <li>
                 <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Homepage">
                   {/* Home icon */}
+                  
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
                   <span className="is-drawer-close:hidden">Homepage</span>
                 </button>
